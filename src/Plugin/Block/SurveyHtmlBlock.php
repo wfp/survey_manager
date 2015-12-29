77b4e0a5997bd3f3ff74be7f0724e3c1935069a5
otarza <otar.zakalashvili@gmail.com>
2015-12-29 14:51:56 +0400

    SurveyHtmlBlock implemented Block class name was renamed, fields implemented according to #1

---
+++ src/Plugin/Block/SurveyHtmlBlock.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\survey_manager\Plugin\Block\SurveyHtmlBlock.
+ */
+
+namespace Drupal\survey_manager\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'Survey HtmlBlock' block.
+ *
+ * @Block(
+ *  id = "survey_htmlblock",
+ *  admin_label = @Translation("Survey HTMLBlock"),
+ * )
+ */
+class SurveyHtmlBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+
+    $form['manage_survey'] = array(
+      '#type'          => 'textfield',
+      '#title'         => $this->t('Manage survey'),
+      '#description'   => $this->t('Optional URL to third party survey tool.'),
+      '#default_value' => isset($this->configuration['manage_survey']) ? $this->configuration['manage_survey'] : "",
+      '#weight'        => '1',
+    );
+
+    $form['html'] = array(
+      '#type'          => 'textarea',
+      '#title'         => $this->t('HTML Code'),
+      '#description'   => $this->t('HTML code of the survey.'),
+      '#default_value' => isset($this->configuration['html']) ? $this->configuration['html'] : "",
+      '#weight'        => '1',
+    );
+
+    $form['active'] = array(
+      '#type'          => 'checkbox',
+      '#title'         => $this->t('Active'),
+      '#description'   => $this->t('Is the survey active?'),
+      '#default_value' => isset($this->configuration['active']) ? $this->configuration['active'] : "",
+      '#weight'        => '2',
+    );
+
+    return $form;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['manage_survey'] = $form_state->getValue('manage_survey');
+    $this->configuration['html']          = $form_state->getValue('html');
+    $this->configuration['active']        = $form_state->getValue('active');
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build                              = [];
+    $build['survey_htmlblock_html']['#markup'] = '<div>' . $this->configuration['html'] . '</div>';
+
+    return $build;
+  }
+
+}
