6f9cbdd2c229b7b310e674c74bb5e75994386e5d
otarza <otar.zakalashvili@gmail.com>
2016-01-06 12:51:43 +0400

    Section names and operations implemented paths, content types and use roles are now represented in sections column. Manage column now containes buttons to manage third party surveys or to edit block.

--- src/Controller/SurveyListController.php
+++ src/Controller/SurveyListController.php
@@ -40,34 +40,58 @@ class SurveyListController extends ControllerBase {
 
     $surveys = array_merge($embed_surveys, $html_surveys);
 
+    $manager     = \Drupal::service('plugin.manager.block');
+    $definitions = $manager->getDefinitions();
+
     foreach ($surveys as $id => $survey) {
+      $plugin_id = $survey->get('settings')['id'];
+
       $surveys_table[$id]['name'] = array(
         '#plain_text' => $survey->get('settings')['label'],
       );
 
+      $sections   = "";
+      $visibility = $survey->getVisibility();
+
+      if (!empty($visibility['request_path'])) {
+        $pages = explode(PHP_EOL, $visibility['request_path']['pages']);
+        $sections .= "Pages : " . implode(", ", $pages) . "</br>";
+      }
+
+      if (!empty($visibility['node_type'])) {
+        $sections .= "Content Types : " . implode(", ",
+            $visibility['node_type']['bundles']) . "</br>";
+      }
+
+      if (!empty($visibility['user_role'])) {
+        $sections .= "Roles: " . implode(", ",
+            $visibility['user_role']['roles']) . "</br>";
+      }
+
       $surveys_table[$id]['sections'] = array(
-        '#plain_text' => "Section Name",
+        '#markup' => $sections,
       );
 
       $surveys_table[$id]['type'] = array(
-        '#plain_text' => "Type of the survey",
+        '#plain_text' => $definitions[$plugin_id]['admin_label']->getUntranslatedString(),
       );
 
-      $surveys_table[$id]['manage'] = array(
+      $surveys_table[$id]['operations'] = array(
         '#type'  => 'operations',
         '#links' => array(),
       );
 
       if (!empty($survey->get('settings')['manage_survey'])) {
-        $surveys_table[$id]['manage']['#links']['manage'] = array(
+        $surveys_table[$id]['operations']['#links']['manage'] = array(
           'title' => t('Manage'),
-          'url'   => $survey->get('settings')['manage_survey'],
+          'url'   => Url::fromUri($survey->get('settings')['manage_survey']),
         );
       }
 
-      $surveys_table[$id]['manage']['#links']['edit'] = array(
+      $surveys_table[$id]['operations']['#links']['edit'] = array(
         'title' => t('Edit'),
-        'url'   => Url::fromRoute('block.admin_display'),
+        'url'   => Url::fromRoute('entity.block.edit_form',
+          array("block" => $id)),
       );
     }
 
