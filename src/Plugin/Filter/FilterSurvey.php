0b5b12e6aa44da34a0c920f8bbb9d73346f5f054
Lasha <lashab@picktek.com>
2016-06-06 10:40:27 +0000

    Fixes the warnings and notices

--- src/Plugin/Filter/FilterSurvey.php
+++ src/Plugin/Filter/FilterSurvey.php
@@ -31,17 +31,18 @@ class FilterSurvey extends FilterBase {
     $new_text = $text;
     $filter_result = new FilterProcessResult($text);
 
-    preg_match('/\[survey\:.+\]/', $text, $result);
-    $token = $result[0];
-    $start = strpos($token, ':') + 1;
-    $length = strpos($token, ']') - $start;
-    $id = substr($token, $start, $length);
-    $block  = Block::load($id);
-    if ($block) {
-      $replace  = $block->get('settings')['html'];
-      $new_text = str_replace($token, $replace, $text);
-      $filter_result->setProcessedText($new_text);
-      $filter_result->setCacheTags($block->getCacheTags());
+    if (preg_match('/\[survey\:.+\]/', $text, $result)) {
+      $token = $result[0];
+      $start = strpos($token, ':') + 1;
+      $length = strpos($token, ']') - $start;
+      $id = substr($token, $start, $length);
+      $block  = Block::load($id);
+      if ($block) {
+        $replace  = $block->get('settings')['html'];
+        $new_text = str_replace($token, $replace, $text);
+        $filter_result->setProcessedText($new_text);
+        $filter_result->setCacheTags($block->getCacheTags());
+      }
     }
 
     return $filter_result;
